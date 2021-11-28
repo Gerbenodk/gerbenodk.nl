2f61384a2221b345db9a23c1ea6ed5dbd51a06fc
Gerben Op den Kamp <gerbenopdenkamp@hotmail.com>
2021-11-28 20:28:51 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,240 @@
+<template>
+  <div class="parallax">
+    <div class="parallax_header parallax_img_0">
+      <img alt="parralax_img_7" class="object-cover lg:object-fill w-full" src="./assets/img/7.png"/>
+    </div>
+    <div class="parallax_header parallax_img_1">
+      <img alt="parralax_img_6" class="object-cover lg:object-fill w-full" src="./assets/img/6.png"/>
+    </div>
+    <div class="parallax_header parallax_img_2">
+      <img alt="parralax_img_5" class="object-cover lg:object-fill w-full" src="./assets/img/5.png"/>
+    </div>
+    <div class="parallax_header parallax_img_3">
+      <div class="-mt-16 my-auto">
+        <h1 class="text-5xl lg:text-7xl text-black text-center z-10 amatic">
+          Gerben Op den Kamp
+        </h1>
+      </div>
+    </div>
+    <div class="parallax_header parallax_img_4">
+      <img alt="parralax_img_2" class="object-cover lg:object-fill w-full" src="./assets/img/2.png"/>
+    </div>
+    <div class="parallax_header parallax_img_5">
+      <img alt="parralax_img_3" class="object-cover lg:object-fill w-full" src="./assets/img/3.png"/>
+    </div>
+    <div class="parallax_header parallax_img_6">
+      <img alt="parralax_img_1" class="object-cover lg:object-fill w-full" src="./assets/img/1.png"/>
+    </div>
+    <div class="parallax_cover bg-white">
+      <div class="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
+        <nav class="border-b border-black w-full h-12">
+          <ul class="text-4xl text-center">
+            <li class="px-4 text-black amatic inline-block"><a href="#projects">Projecten</a></li>
+            <li class="px-4 text-black amatic inline-block"><a href="#contact">Contact</a></li>
+          </ul>
+        </nav>
+        <section>
+          <div class="grid grid-cols-1 md:grid-cols-2 gap-8 my-8 ">
+            <div>
+              <div class="svg">
+                <embed class="m-auto md:72 lg:w-80 xl:96" src="./src/assets/svg/1.svg">
+              </div>
+            </div>
+            <div>
+              <p class="text-lg mb-4">
+                <!--                My day starts with going through the latest tech developments. As an ICT-Media design-->
+                <!--                student, I like to use my expertise in challenging assignments and to come to a solution-->
+                <!--                both individually and in a team. At the moment I use this for various projects besides-->
+                <!--                my study. I am driven to further refine my knowledge and apply it in practice. Because-->
+                <!--                of my proactive and critical attitude, I dare to ask questions and set priorities where-->
+                <!--                necessary.-->
+                Mijn dag begint met het doornemen van de laatste ontwikkelingen op techgebied. Als ICT-Media design
+                student vind ik het prettig om mijn expertise binnen uitdagende opdrachten in te zetten en zowel
+                individueel als in teamverband tot een oplossing te komen. Op het moment benut ik dit ook buiten de
+                studie om met verschillende projecten. Ik ben gedreven om mijn geleerde kennis verder te verfijnen en in
+                de praktijk toe te passen. Door mijn proactieve en kritische houding durf ik vragen te stellen en
+                prioriteiten te stellen waar nodig.
+              </p>
+              <p class="font-bold mb-2">
+                Meer over mij:
+              </p>
+              <a target="_blank" class="px-6 py-2 text-black border border-black hover:text-white hover:bg-black " href="./assets/pdf/CV.pdf">
+                Curriculum vitae
+              </a>
+            </div>
+          </div>
+        </section>
+        <section id="projects" class="my-16">
+          <div class="border-b border-black">
+            <h1 class="text-4xl amatic">Projecten</h1>
+          </div>
+          <div class="grid grid-cols-1 md:grid-cols-2 gap-8 my-12">
+            <div>
+              <h3 class="text-lg font-bold">
+                Beyond Selection
+              </h3>
+              <p>
+                This website I created because i always lost my links. Therefor this concept was
+                created and will allow me to keep my links on all platforms close.
+              </p>
+            </div>
+            <div>
+              <a href="https://beyond-selection.nl/" target="_blank"><img class="img"
+                                                                          src="./assets/img/beyondselection.png"
+                                                                          alt="beyondselection"></a>
+            </div>
+          </div>
+
+          <div class="grid grid-cols-1 md:grid-cols-2 gap-8 my-12">
+            <div>
+              <a href="https://ippon-personeelsdiensten.nl/" target="_blank"><img class="img"
+                                                                                  src="./assets/img/ipponpersoneeldiensten.png"
+                                                                                  alt="ipponpersoneelsdiensten"></a>
+            </div>
+            <div class="order-first md:order-last">
+              <h3 class="text-lg font-bold">
+                Ippon Personeelsdiensten
+              </h3>
+              <p>
+                This website I created for my first customer. The backend of this website I made an
+                CMS where you can monitor google analytics and email traffic.
+              </p>
+            </div>
+          </div>
+          <div class="grid grid-cols-1 md:grid-cols-2 gap-8 my-12">
+            <div>
+              <h3 class="text-lg font-bold">
+                Kant en Klaar Programma
+              </h3>
+              <p>
+                This website I created for my first customer. The backend of this website I made an
+                CMS where you can monitor google analytics and email traffic.
+              </p>
+            </div>
+            <div>
+              <a href="https://www.kantenklaarprogramma.nl/" target="_blank"><img class="img"
+                                                                                  src="./assets/img/kant.png"
+                                                                                  alt="kantenklaarprogramma"></a>
+            </div>
+          </div>
+          <div class="grid grid-cols-1 md:grid-cols-2 gap-8 my-12">
+            <div>
+              <a href="https://www.ownhomepage.com/home" target="_blank"><img class="img"
+                                                                              src="./assets/img/ownhomepage.png"
+                                                                              alt="ownhomepage"></a>
+            </div>
+            <div class="order-first md:order-last">
+              <h3 class="text-lg font-bold">
+                Ownhomepage
+              </h3>
+              <p>
+                This website I created because i always lost my links. Therefor this concept was
+                created and will allow me to keep my links on all platforms close.
+              </p>
+            </div>
+          </div>
+
+          <div class="my-16 relative">
+            <h3 class="font-bold text-lg absolute text-white m-4">Digital Snowboarding</h3>
+            <img alt="Snowboarding animation Img converted to SVG" id="img" src="./assets/svg/1_000.svg"/>
+          </div>
+        </section>
+        <section id="contact" class="my-12">
+          <div class="border-b border-black">
+            <h1 class="text-4xl amatic">Contact</h1>
+          </div>
+          <p>
+            <a href="mailto:gerbenopdenkamp@hotmail.com?Subject=Hi%20How are you doing">
+              <i class="fa fa-envelope"
+                 aria-hidden="true"></i>:
+              gerbenopdenkamp@hotmail.com</a>
+            <br>
+            <a target="_blank" href="https://www.linkedin.com/in/gerbenodk/">
+              <i class="fa fa-linkedin-square"
+                 aria-hidden="true"></i>
+              gerbenodk</a>
+          </p>
+        </section>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Main",
+  data() {
+    return {
+      images: [
+        "./src/assets/svg/1_001.svg",
+        "./src/assets/svg/1_002.svg",
+        "./src/assets/svg/1_003.svg",
+        "./src/assets/svg/1_004.svg",
+        "./src/assets/svg/1_005.svg",
+        "./src/assets/svg/1_006.svg",
+        "./src/assets/svg/1_007.svg",
+        "./src/assets/svg/1_008.svg",
+        "./src/assets/svg/1_009.svg",
+        "./src/assets/svg/1_010.svg",
+        "./src/assets/svg/1_011.svg",
+        "./src/assets/svg/1_012.svg",
+        "./src/assets/svg/1_013.svg",
+        "./src/assets/svg/1_014.svg",
+        "./src/assets/svg/1_015.svg",
+        "./src/assets/svg/1_016.svg",
+        "./src/assets/svg/1_017.svg",
+        "./src/assets/svg/1_018.svg",
+        "./src/assets/svg/1_019.svg",
+        "./src/assets/svg/1_020.svg",
+        "./src/assets/svg/1_021.svg",
+        "./src/assets/svg/1_023.svg",
+        "./src/assets/svg/1_024.svg",
+        "./src/assets/svg/1_025.svg",
+        "./src/assets/svg/1_026.svg",
+        "./src/assets/svg/1_027.svg",
+        "./src/assets/svg/1_028.svg",
+        "./src/assets/svg/1_029.svg",
+        "./src/assets/svg/1_030.svg",
+        "./src/assets/svg/1_031.svg",
+        "./src/assets/svg/1_032.svg",
+        "./src/assets/svg/1_033.svg",
+        "./src/assets/svg/1_034.svg",
+        "./src/assets/svg/1_035.svg",
+        "./src/assets/svg/1_036.svg",
+        "./src/assets/svg/1_037.svg",
+        "./src/assets/svg/1_038.svg",
+        "./src/assets/svg/1_039.svg",
+        "./src/assets/svg/1_040.svg",
+        "./src/assets/svg/1_041.svg",
+        "./src/assets/svg/1_042.svg",
+        "./src/assets/svg/1_043.svg",
+        "./src/assets/svg/1_044.svg",
+        "./src/assets/svg/1_045.svg",
+        "./src/assets/svg/1_046.svg",
+        "./src/assets/svg/1_047.svg",
+        "./src/assets/svg/1_048.svg",
+        "./src/assets/svg/1_049.svg",
+        "./src/assets/svg/1_050.svg",
+        "./src/assets/svg/1_051.svg",
+      ],
+      x: 1,
+    }
+
+  },
+  methods: {
+    displayNextImage() {
+      this.x = (this.x === this.images.length - 1) ? 1 : this.x + 1;
+      document.getElementById("img").src = this.images[this.x];
+    },
+    start() {
+      setTimeout(() => {
+        setInterval(() => {
+          this.displayNextImage()
+        }, 100);
+      }, 1000);
+    }
+  },
+  created() {
+    this.start();
+  },
+}
+</script>
